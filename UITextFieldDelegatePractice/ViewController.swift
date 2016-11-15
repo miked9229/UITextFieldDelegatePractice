4fd8b10bb5ba963c92bcd12a389dddba1725da9a
Michael Doroff <michaeldoroff@Michaels-MacBook-Air.local>
2016-11-14 19:47:15 -0600

    Initial Commit

---
+++ UITextFieldDelegatePractice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UITextFieldDelegatePractice
+//
+//  Created by Michael Doroff on 11/14/16.
+//  Copyright © 2016 Michael Doroff. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
